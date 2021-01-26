2bd2d5360a96be40377afa4cc6477648265f97a1
Ervin Abliamitov <rwin321321@gmail.com>
2021-01-26 12:53:54 +0200

    initializtaion of bootstrap web_site

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Header.css'
+
+export const Header = () => {
+	return (
+		<div className = 'jumbotron header'>
+			<div className = 'container'>
+				<div>
+					<div>
+						Header name
+					</div>
+					<div></div>
+				</div>
+			</div>
+		</div>
+	)
+}
